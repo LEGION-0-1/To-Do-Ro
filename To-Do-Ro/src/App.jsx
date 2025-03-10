f7dc661e788227a4a02079e994a7fd83e7dc2e95
LEGION-0-1 <the.dilpreet.1@gmail.com>
2025-03-10 15:44:27 +0530

    color preset system completed

--- To-Do-Ro/src/App.jsx
+++ To-Do-Ro/src/App.jsx
@@ -1,35 +1,41 @@
-import React, { useState } from 'react';
-import './App.css';
-import ToDo from './toDo.jsx';
-import Timer from './Timer.jsx';
-import Preset from './Presets.jsx';
+import React, { useState } from "react";
+import "./App.css";
+import ToDo from "./toDo.jsx";
+import Timer from "./Timer.jsx";
+import Preset from "./Presets.jsx";
 
 function App() {
   const [showPresets, setShowPresets] = useState(false);
+  const [bgColor, setBgColor] = useState("#242424");
+  const [textColor, setTextColor] = useState("white");
 
   function show() {
-    setShowPresets(prevState => !prevState);
+    setShowPresets((prevState) => !prevState);
+  }
+
+  function handleClick(Text, BG) {
+    setBgColor(BG);
+    setTextColor(Text);
   }
 
   return (
-    <>
+    <div id="page" style={{ backgroundColor: bgColor, color: textColor}}>
       <header>
         <nav>
           <h2>To-Do-Ro</h2>
-          <button onClick={show}>
-            Color
-          </button>
+          <button onClick={show}>Color</button>
           {showPresets && (
-            <div style={{ display: 'flex' }} id="color-presets">
-              <Preset primary="red" secondary="black" />
-              <Preset primary="white" secondary="black" />
+            <div style={{ display: "flex" }} id="color-presets">
+              <Preset primary="red" secondary="black" onClick={() => handleClick("red", "black")} />
+              <Preset primary="white" secondary="black" onClick={() => handleClick("white", "black")} />
+              <Preset primary="white" secondary="black" onClick={() => handleClick("white", "#242424")} />
             </div>
           )}
         </nav>
       </header>
-      <Timer />
-      <ToDo />
-    </>
+      <Timer style={{borderColor: textColor}} />
+      <ToDo style={{}} />
+    </div>
   );
 }
 
--- To-Do-Ro/src/Presets.jsx
+++ To-Do-Ro/src/Presets.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Preset({ onClick, primary, secondary }) {
     return (
-        <button onClick={() => onClick(primary, secondary)}>
+        <button style={{margin: 10}}onClick={() => onClick(primary, secondary)}>
             <div style={{ backgroundColor: primary, display: 'inline-block', width: '15px', height: '30px', borderRadius:'15px 0 0 15px' }}></div>
             <div style={{ backgroundColor: secondary, display: 'inline-block', width: '15px', height: '30px', borderRadius:'0 15px 15px 0' }}></div>
         </button>
